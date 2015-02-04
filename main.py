3c097170f7aba36231b382b8d3fa0cbed0cb18bf
Bo Zhu <github@bozhu.me>
2015-02-04 12:29:52 -0500

    use redis as key-value storage for now

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python
 
-
-import cPickle
 import requests
 
 from parser import EPrintParser
 from tweet import tweet
+from storage import retrieve_data, store_data
 from config import HTTP_HEADERS, sentry_client
 
 
 @sentry_client.capture_exceptions
 def main():
-    result = requests.get(
+    resp = requests.get(
         'http://eprint.iacr.org/eprint-bin/search.pl?last=31&title=1',
         headers=HTTP_HEADERS
     )
 
-    if result.status_code != 200:
-        msg = 'ret: ' + str(result.status_code)   \
-            + '\n\n' + result.text
+    if resp.status_code != 200:
+        msg = 'request failed: ' + str(resp.status_code) \
+            + '\n\n' + resp.text
         raise Exception(msg)
 
     my_parser = EPrintParser()
-    curr_list = my_parser.feed(result.text)
+    curr_list = my_parser.feed(resp.text)
 
-    prev_data = retrieve_data()
-    if prev_data is not None:
-        prev_list = cPickle.loads(prev_data)
+    prev_list = retrieve_data()
+    if prev_list is None \
+            or not isinstance(prev_list, list) \
+            or len(prev_list) == 0:
+        store_data(curr_list)
+
+    else:
         list_updated = [i for i in curr_list if i not in prev_list]
         if len(list_updated):
             list_untweeted = tweet(list_updated)
             list_to_save = [i for i in curr_list if i not in list_untweeted]
-            store_data(cPickle.dumps(list_to_save))
-    else:
-        store_data(cPickle.dumps(curr_list))
+            store_data(list_to_save)
 
 
 if __name__ == '__main__':
--- storage.py
+++ storage.py
@@ -1,2 +1,34 @@
 #!/usr/bin/env python
 
+import os
+import redis
+import urlparse
+import pickle
+
+
+redis_url = urlparse.urlparse(
+    os.environ.get('REDISCLOUD_URL',
+                   'redis://localhost:6379'))
+redis_storage = redis.Redis(
+    host=redis_url.hostname,
+    port=redis_url.port,
+    password=redis_url.password)
+
+
+def retrieve_data():
+    data = redis_storage.get('pre-data')
+    if data is not None:
+        data = pickle.loads(data)
+    return data
+
+
+def store_data(data):
+    return redis_storage.set('pre-data', pickle.dumps(data))
+
+
+if __name__ == '__main__':
+    # store_data(1)
+    res = retrieve_data()
+    print type(res), res
+
+    print redis_storage.get('not exist')
